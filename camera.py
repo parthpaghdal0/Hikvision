d94409aa9cdf0a55e2ea526df547bd1480163563
Parth <128666215+parthpaghdal0@users.noreply.github.com>
2024-04-11 01:03:53 -0700

    add hotkey

--- camera.py
+++ camera.py
@@ -24,51 +24,65 @@ class Camera:
         url = generateUrl(ip, username, password)
 
         try:
-            requests.put(url, timeout=10)
+            requests.delete(url, timeout=10)
             return True
         except:
             return False
 
     @staticmethod
     def writeTextOverlay(ip, username, password, text1, text2):
+        text1_1 = text1[:44]
+        text1_2 = text1[44:]
+        text2_1 = text2[:44]
+        text2_2 = text2[44:]
+        
+        text1_1 = text1_1.replace("&", "&amp;")
+        text1_2 = text1_2.replace("&", "&amp;")
+        text2_1 = text2_1.replace("&", "&amp;")
+        text2_2 = text2_2.replace("&", "&amp;")
+
+        print(text1_1, text1_2)
+        print(text2_1, text2_2)
+
         body = '\
         <TextOverlayList version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema">\
             <TextOverlay version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema">\
                 <id>1</id>\
                 <enabled>true</enabled>\
                 <positionX>0</positionX>\
-                <positionY>48</positionY>\
-                <displayText>51-510504, 20YD DISPOSAL C&amp;D INBD TN, </displayText>\
+                <positionY>160</positionY>\
+                <displayText>%s</displayText>\
             </TextOverlay>\
             <TextOverlay version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema">\
                 <id>2</id>\
                 <enabled>true</enabled>\
-                <positionX>400</positionX>\
-                <positionY>48</positionY>\
-                <displayText>SW WASTE ROLLOFF, 36040, 32220</displayText>\
+                <positionX>0</positionX>\
+                <positionY>128</positionY>\
+                <displayText>%s</displayText>\
             </TextOverlay>\
             <TextOverlay version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema">\
                 <id>3</id>\
                 <enabled>true</enabled>\
                 <positionX>0</positionX>\
-                <positionY>0</positionY>\
-                <displayText>51-510504, 20YD DISPOSAL C&amp;D INBD YD, SW WASTE ROLLOFF, 0, 0</displayText>\
+                <positionY>64</positionY>\
+                <displayText>%s</displayText>\
             </TextOverlay>\
             <TextOverlay version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema">\
                 <id>4</id>\
                 <enabled>true</enabled>\
-                <positionX>400</positionX>\
-                <positionY>0</positionY>\
-                <displayText>SW WASTE ROLLOFF, 0, 0</displayText>\
+                <positionX>0</positionX>\
+                <positionY>32</positionY>\
+                <displayText>%s</displayText>\
             </TextOverlay>\
         </TextOverlayList>\
-        '
+        ' % (text1_1, text1_2, text2_1, text2_2)
         url = generateUrl(ip, username, password)
 
         try:
-            requests.put(url, body, timeout=10)
-            return True
+            response = requests.put(url, body, timeout=10)
+            if response.status_code == 200:
+                return True
+            else:
+                return False
         except:
             return False
-
-
--- widget.py
+++ widget.py
@@ -2,10 +2,12 @@
 
 import os
 import keyboard
+import pyodbc
+from datetime import datetime
 
 from widget_ui import Ui_Widget
 from PyQt6.QtWidgets import QWidget, QMessageBox
-from PyQt6.QtCore import QSettings, pyqtSlot, pyqtSignal
+from PyQt6.QtCore import QSettings, QTimer, pyqtSlot, pyqtSignal
 
 from cryptography import validate_license
 from camera import Camera
@@ -15,6 +17,7 @@ ini_file_path = os.path.join(program_data_dir, 'hikvisionCRM', 'settings.ini')
 
 class Widget(QWidget, Ui_Widget):
     hotkeySignal = pyqtSignal()
+    timer = QTimer()
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -25,6 +28,8 @@ class Widget(QWidget, Ui_Widget):
 
         self.hotkeySignal.connect(self.on_hotkey_slot)
 
+        timer = QTimer()
+
     def showEvent(self, event):
         settings = QSettings(ini_file_path, QSettings.Format.IniFormat)
 
@@ -34,10 +39,17 @@ class Widget(QWidget, Ui_Widget):
         self.le_camera_password.setText(settings.value("password"))
         settings.endGroup()
 
+        settings.beginGroup("odbc")
+        self.le_odbc_ip.setText(settings.value("ip"))
+        self.le_odbc_username.setText(settings.value("username"))
+        self.le_odbc_password.setText(settings.value("password"))
+        settings.endGroup()
+
         settings.beginGroup("settings")
         self.le_settings_userid.setText(settings.value("userid", "S5NICK"))
+        self.le_settings_companyid.setText(settings.value("companyid"))
         self.le_settings_delay.setText(settings.value("delay", "5"))
-        self.l_settings_hotkey.setText(settings.value("hotkey", "Ctrl+Shift+P"))
+        self.l_settings_hotkey.setText(settings.value("hotkey", "Ctrl+Shift+Q"))
         self.te_settings_license.setPlainText(settings.value("license"))
         settings.endGroup()
 
@@ -56,8 +68,20 @@ class Widget(QWidget, Ui_Widget):
         if self.le_camera_password.text() == "":
             QMessageBox.warning(self, 'Warning', 'Please input camera password.')
             return
+        if self.le_odbc_ip.text() == "":
+            QMessageBox.warning(self, 'Warning', 'Please input ODBC ip address.')
+            return
+        if self.le_odbc_username.text() == "":
+            QMessageBox.warning(self, 'Warning', 'Please input ODBC username.')
+            return
+        if self.le_odbc_password.text() == "":
+            QMessageBox.warning(self, 'Warning', 'Please input ODBC password.')
+            return
         if self.le_settings_userid.text() == "":
             QMessageBox.warning(self, 'Warning', 'Please input user id.')
+            return
+        if self.le_settings_companyid.text() == "":
+            QMessageBox.warning(self, 'Warning', 'Please input company id.')
             return
         if self.le_settings_delay.text() == "":
             QMessageBox.warning(self, 'Warning', 'Please input delay.')
@@ -69,6 +93,12 @@ class Widget(QWidget, Ui_Widget):
             QMessageBox.warning(self, 'Warning', 'Your license is invalid.')
             return
 
+        settings.beginGroup("odbc")
+        settings.setValue("ip", self.le_odbc_ip.text())
+        settings.setValue("username", self.le_odbc_username.text())
+        settings.setValue("password", self.le_odbc_password.text())
+        settings.endGroup()
+
         settings.beginGroup("camera")
         settings.setValue("ip", self.le_camera_ip.text())
         settings.setValue("username", self.le_camera_username.text())
@@ -77,6 +107,7 @@ class Widget(QWidget, Ui_Widget):
 
         settings.beginGroup("settings")
         settings.setValue("userid", self.le_settings_userid.text().upper())
+        settings.setValue("companyid", self.le_settings_companyid.text())
         settings.setValue("delay", self.le_settings_delay.text())
         settings.setValue("hotkey", self.l_settings_hotkey.text())
         settings.setValue("license", self.te_settings_license.toPlainText())
@@ -94,11 +125,73 @@ class Widget(QWidget, Ui_Widget):
         else:
             QMessageBox.warning(self, 'Warning', 'Connection was unsuccessful.')
 
+    @pyqtSlot()
+    def on_pb_odbc_test_clicked(self):
+        ip = self.le_odbc_ip.text()
+        username = self.le_odbc_username.text()
+        password = self.le_odbc_password.text()
+        try:
+            url = 'DRIVER={iSeries Access ODBC Driver};SYSTEM=%s;DATABASE=IWSE4S5;UID=%s;PWD=%s' % (ip, username, password)
+            pyodbc.connect(url)
+            QMessageBox.information(self, 'Information', 'Connection was successful.')
+        except:
+            QMessageBox.warning(self, 'Warning', 'Connection was unsuccessful.')
+
     def on_hotkey_pressed(self, event):
         if keyboard.is_pressed('CTRL+SHIFT+Q'):
             self.hotkeySignal.emit()
 
+    def parseResult(self, row):
+        result = '%s-%s, %s, %s, %s, %s, %s' % (row[0], row[1], str(row[2]).strip(), str(row[3]).strip(), row[4], row[5], str(row[6]).strip())
+        return result
+    
+    @pyqtSlot()
+    def on_timeout(self):
+        ip = self.le_camera_ip.text()
+        username = self.le_camera_username.text()
+        password = self.le_camera_password.text()
+
+        Camera.clearTextOverlay(ip, username, password)
+        print("Clean was successful")
+
     @pyqtSlot()
     def on_hotkey_slot(self):
-        print('hotkey pressed')
-        pass
+        ip = self.le_odbc_ip.text()
+        username = self.le_odbc_username.text()
+        password = self.le_odbc_password.text()
+        print("Running Hotkey...")
+        try:
+            url = 'DRIVER={iSeries Access ODBC Driver};SYSTEM=%s;DATABASE=IWSE4S5;UID=%s;PWD=%s' % (ip, username, password)
+            conn = pyodbc.connect(url)
+            cursor = conn.cursor()
+
+            cursor.execute('SELECT STCOMP, Max(STTCKT) AS MaxOfSTTCKT, STUSER\
+                FROM Iwse4s5.SCTRN\
+                GROUP BY STCOMP, STUSER, STDATE\
+                HAVING ((STUSER=?) AND (STDATE=?))', self.le_settings_userid.text(), datetime.today().strftime('%Y%m%d')) #datetime.today().strftime('%Y%m%d')
+            rows = cursor.fetchall()
+            STTTCKT = rows[0][1]
+            print("Ticket Number is = " + str(STTTCKT))
+
+            cursor.execute('SELECT stcomp, sttckt, stdesc, cblnam, STGROS AS Gross, STTARE AS Tare, STVEH1\
+                FROM Iwse4s5.CUST INNER JOIN Iwse4s5.SCTRN ON (Iwse4s5.CUST.CCUST# = Iwse4s5.SCTRN.STCUST) AND (Iwse4s5.CUST.CCMPNY = Iwse4s5.SCTRN.STCOMP)\
+                WHERE (((Iwse4s5.SCTRN.STCOMP)=?) AND ((Iwse4s5.SCTRN.STTCKT)=?))', self.le_settings_companyid.text(), STTTCKT)
+            rows = cursor.fetchall()
+            text1 = self.parseResult(rows[0])
+            text2 = self.parseResult(rows[1])
+
+            ip = self.le_camera_ip.text()
+            username = self.le_camera_username.text()
+            password = self.le_camera_password.text()
+            if Camera.writeTextOverlay(ip, username, password, text1, text2) == True:
+                print("Write was successful")
+            else:
+                print("Write failed")
+
+            cursor.close()
+            conn.close()
+
+            delay = int(self.le_settings_delay.text()) * 1000
+            self.timer.singleShot(delay, self.on_timeout)
+        except Exception as e:
+            print("Failed to run hotkey.", e)
